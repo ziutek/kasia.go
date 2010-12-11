92ad984fb9119c0d3549aa8dfd8a9fdc3c2e11e6
Michał Derkacz <michal@Lnet.pl>
2010-12-11 22:52:03 +0100

    Added some tests

--- kasia_test.go
+++ kasia_test.go
@@ -4,38 +4,84 @@ import "testing"
 
 type Test struct {
     txt    string
+    expect string
     strict bool
     ctx    interface{}
-    expect string
 }
 
-var tests = []Test{
+
+var (
+
+f1 = func() int { return 0 }
+
+tests = []Test{
 {
-    "Łódź, róża, łąka", true,
+    // Tekst bez zmiennych
+    `Łódź, róża, łąka, $'$"$$.`,
+    `Łódź, róża, łąka, '"$.`, true,
     nil,
-    "Łódź, róża, łąka",
 },{
-    "$aa, $bc(1)", true,
+    // map: Zmienna i funkcja
+    `$aa, $bc(1)`,
+    `Ala, 2`, true,
     map[string]interface{}{"aa": "Ala", "bc": func(i int)int{return 2*i}},
-    "Ala, 2",
 },{
-    `$["aa"], $['bc'](1)`, true,
+    // map: Zmienna i funkcja, indeksy
+    `$["aa"], $['bc'](1)`,
+    `Ala, 2`, true,
     map[string]interface{}{"aa": "Ala", "bc": func(i int)int{return 2*i}},
-    "Ala, 2",
+},{
+    // struct: 'if', funkcja zwracajaca funkcje, complex
+    `$if f(1)('str')(2.1) == c: OK $end $f(a)("eeee")(b) $if b==2.1:$a$b$c$end`,
+    ` OK  (6+2.1i) 22.1(4+2.1i)`, true,
+    struct {
+        f func(int)func(string)func(float)complex
+        a int
+        b float
+        c complex
+    } {
+        func(i int) func(string)func(float)complex {
+            return func(s string) func(float)complex {
+                return func(f float) complex {
+                    return cmplx(float(i + len(s)), f)
+                }
+            }
+        },
+        2,
+        2.1,
+        cmplx(4, 2.1),
+    },
+},{
+    // struct: 'if' jednoargumentowy, funkcja bez argumentow
+    `$if f: $f() $end $if f(): First $else: Second $end`,
+    ` 0   Second `, true,
+    struct {f *func()int}{&f1},
+},{
+    // func:
+    `$((((((((1)))))))) $(0)`,
+    `9 1`, true,
+    func (i int) int {i++; return i},
+},{
+    // slice:
+    `$[0], $[1], $[2], $[-1], $[-2], $[-3]`,
+    `A, B, C, C, B, A`, true,
+    []string {"A", "B", "C"},
 },
 }
+
+)
 
 func TestAll(t *testing.T) {
     for _, te := range tests {
         out, err := RenderString(te.txt, te.strict, te.ctx)
         strict := ""
         if te.strict {
-            strict = "/strict"
+            strict = "!"
         }
         if err != nil {
-            t.Fatalf("|%s|%s err: %s", te.txt, strict, err)
+            t.Fatalf("`%s`%s err: %s", te.txt, strict, err)
         } else if out != te.expect {
-            t.Fatalf("|%s|%s out:|%s| expect:|%s|",
+            t.Fatalf("`%s`%s out `%s` expect `%s`",
                 te.txt, strict, out, te.expect)
         }
     }
