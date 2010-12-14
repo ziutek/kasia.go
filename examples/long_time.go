6de262c8a4488fda07bab004aa34ff6fe8352872
Michał Derkacz <michal@Lnet.pl>
2010-12-14 15:28:20 +0100

    Added new example (performance test, memory leaks)

---
+++ examples/long_time.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "kasia"
+)
+
+type ContextData struct {
+    A   int
+    B   string
+    C   bool
+    D   *[]interface{}
+    E   map[string]interface{}
+    F   func(int) interface{}
+    G   *ContextData
+}
+
+func (cd ContextData) M1() int {
+    return cd.A
+}
+
+func (cd *ContextData) M2(s string) bool {
+    return cd.B == s
+}
+
+
+func main() {
+    data_func := func(a int) interface{} {
+        if a == 0 {
+            return func(s string, i int) string {
+                return fmt.Sprintf(">>%s, %d<<", s, i)
+            }
+        }
+        return "'a != 0'"
+    }
+
+    data_slice := []interface{}{
+        1,
+        func(f float) string {
+            return fmt.Sprintf("First value >%f<.", f)
+        },
+        "text in slice",
+        false,
+    }
+
+    data_struct := ContextData{
+        2,
+        "A",
+        true,
+        &data_slice, //data_slice[:len(data_slice)-1],
+        nil,
+        data_func,
+        nil,
+    }
+
+    data_map := map[string]interface{}{
+        "a":    1,
+        "b":    "'&......................text in map....................&'\n",
+        "c":    false,
+        "d":    data_slice[1:],
+        "e":    nil,
+        "f":    &data_func,
+        "g":    &data_struct,
+    }
+
+    data_map["e"] = data_map
+    data_struct.E = data_map
+    data_struct.G = &data_struct
+
+    bgn := time.Nanoseconds()
+    tpl, err := kasia.ParseFile("long_time.kt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    tpl.Strict = true
+    end := time.Nanoseconds()
+    fmt.Printf("Parsing: %d us\n", (end-bgn) / 1000)
+
+    n := 1000
+    for ii := 1;; ii++ {
+        bgn := time.Nanoseconds()
+        txt := ""
+        for ii := 0; ii < n; ii++ {
+            txt = tpl.Render(data_struct)
+            if err != nil  {
+                fmt.Println(err)
+                return
+            }
+        }
+        end := time.Nanoseconds()
+        fmt.Printf("Rendered %d*%d B: %.0f r/s, %.0f kB/s\n",
+            ii*n, len(txt), 1e9 * float(n) / float(end-bgn),
+            1e6 * float(len(txt) * n) / float(end-bgn))
+
+    }
+}
---
+++ examples/long_time.kt
@@ -0,0 +1,33 @@
+START
+
+$F(0)("$$$A $"and$" $$$["A"]", 1)
+$['F'](0)('$$$A "and" $$$['A']', 1)
+
+$for i, v in D:
+  $i: $if i == 1:
+    $v(1.1)
+  $else:
+    D[$i] = $v
+  $end
+$else:
+  D == nil or len(D) == 0
+$end
+
+$A    ==  $[0]
+$B    ==  $[1]
+$C    ==  $[2]
+$D[A] ==  $[3][A]   // $A is integer used as index
+$:E.a  ==  $:[4].a
+$:E.b  ==  $:[4].b
+$G.A  ==  $[6][0]
+$G.A  ==  $[-1][0]  // Index can be negative.
+
+$:M1
+$M1()
+$G.M2("A")
+
+eee${A}eee uuu$:{A}uuu
+
+$# Comment #$
+
+END
